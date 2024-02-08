aeb2d5d5f2cf8bfa24d29cef1e0f81d2ea5aec79
gregmaax <greg.d.972@gmail.com>
2024-02-08 18:32:12 +0100

    register / sign in components

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,8 @@
 <template>
   <h1>Home component</h1>
   <Alert />
+  <UCard class="mx-auto max-w-[1280px]">
+
+  </UCard>
 
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { z } from 'zod'
+import type { FormSubmitEvent } from '#ui/types'
+
+const schema = z.object({
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters')
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive({
+  email: undefined,
+  password: undefined
+})
+
+async function onSubmit (event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data)
+}
+</script>
+
+<template>
+  <UCard class="max-w-[500px] mx-auto mt-[100px]">
+    <template #header>
+      <h1>Sign in</h1>
+    </template>
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+      <UFormGroup label="Email" name="email">
+        <UInput v-model="state.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password">
+        <UInput v-model="state.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit">
+        Sign in
+      </UButton>
+    </UForm>
+    <template #footer>
+      <p>Don't have an account yet ? <ULink to="/register" inactive-class="text-primary">Register</ULink></p>
+    </template>
+  </UCard>
+
+</template>
